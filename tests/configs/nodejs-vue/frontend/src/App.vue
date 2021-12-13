efc667d4d92c9affda7f18442ae19a72ea66b6d3
Grady Rogers <gradytrogers@gmail.com>
2021-12-13 17:10:44 -0500

    Vue example (#9)
    * Include vue example
    
    * read me

---
+++ tests/configs/nodejs-vue/frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="get-data">
+    <img alt="Vue logo" src="./assets/vue-icon.png">
+    <h1>Get some API Data</h1>
+    <button v-on:click="getHelloData()">Get Hello Data</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+const { VUE_APP_API_BASE_URL } = process.env;
+export default {
+  data() {
+    return {
+      message: '',
+    };
+  },
+  methods: {
+    async getHelloData() {
+      const { data: respBody } = await axios.get(`${VUE_APP_API_BASE_URL}/hello`);
+    const { message } = respBody;
+    this.message = message;
+   },
+  },
+}
+</script>
+
+<style scoped>
+#get-data {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
